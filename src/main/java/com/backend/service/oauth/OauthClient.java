e874640c8cbe4e4e318b5c1e4bc2d835709d669e
Jae-Young98 <ygy0102@gmail.com>
2025-06-26 01:06:34 +0900

    feat: 구글 Oauth AccessToken을 이용해 사용자 정보를 파싱하는 기능 추가

--- src/main/java/com/backend/service/oauth/OauthClient.java
+++ src/main/java/com/backend/service/oauth/OauthClient.java
@@ -1,6 +1,7 @@
 package com.backend.service.oauth;
 
 import com.backend.dto.request.GoogleOauthLoginRequest;
+import com.backend.dto.response.GoogleOauthInfoApiResponse;
 import com.backend.dto.response.GoogleOauthTokenResponse;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,21 @@ public class OauthClient {
     private final RestClient restClient;
     private final OauthRequestProperties oauthRequestProperties;
 
-    public GoogleOauthTokenResponse requestToken(GoogleOauthLoginRequest request) {
+    public GoogleOauthInfoApiResponse requestOauthInfo(GoogleOauthLoginRequest request) {
+        GoogleOauthTokenResponse googleOauthTokenResponse = requestToken(request);
+
+        String userInfoRequestUri = oauthRequestProperties.getUserInfoUri();
+        String header = "Authorization";
+        String headerValue = "Bearer " +googleOauthTokenResponse.access_token();
+
+        return restClient.get()
+                .uri(userInfoRequestUri)
+                .header(header, headerValue)
+                .retrieve()
+                .body(GoogleOauthInfoApiResponse.class);
+    }
+
+    private GoogleOauthTokenResponse requestToken(GoogleOauthLoginRequest request) {
         String tokenRequestUri = oauthRequestProperties.getTokenRequestUri();
         Map<String, String> requestBody = oauthRequestProperties.createTokenRequestBody(request);
 
