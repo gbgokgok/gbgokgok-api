2b171da32ae93e69ee8ce585c176b0ee8bcda6e8
Jae-Young98 <ygy0102@gmail.com>
2025-07-06 23:22:49 +0900

    feat: jwt 토큰 발급 기능 추가

---
+++ src/main/java/com/backend/service/jwt/JwtTokenProperties.java
@@ -0,0 +1,44 @@
+package com.backend.service.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProperties {
+
+    protected final String TOKEN_TYPE = "type";
+
+    private final String secretKey;
+    private final long accessTokenExpirationMillis;
+    private final long refreshTokenExpirationMillis;
+    private final long signupTokenExpirationMillis;
+
+    public JwtTokenProperties(
+            @Value("${jwt.secret-key}") String secretKey,
+            @Value("${jwt.accessToken-expiration-millis}") long accessTokenExpirationMillis,
+            @Value("${jwt.refreshToken-expiration-millis}") long refreshTokenExpirationMillis,
+            @Value("${jwt.signupToken-expiration-millis}") long signupTokenExpirationMillis) {
+        this.secretKey = secretKey;
+        this.accessTokenExpirationMillis = accessTokenExpirationMillis;
+        this.refreshTokenExpirationMillis = refreshTokenExpirationMillis;
+        this.signupTokenExpirationMillis = signupTokenExpirationMillis;
+    }
+
+    public long getAccessTokenExpirationMillis() {
+        return accessTokenExpirationMillis;
+    }
+
+    public long getRefreshTokenExpirationMillis() {
+        return refreshTokenExpirationMillis;
+    }
+
+    public long getSignupTokenExpirationMillis() {
+        return signupTokenExpirationMillis;
+    }
+
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+}
---
+++ src/main/java/com/backend/service/jwt/JwtTokenProvider.java
@@ -0,0 +1,64 @@
+package com.backend.service.jwt;
+
+import com.backend.domain.member.Member;
+import com.backend.dto.response.GoogleOauthInfoApiResponse;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import java.util.Date;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private static final String SIGNUP_SUBJECT = "signup";
+
+    private final JwtTokenProperties jwtTokenProperties;
+
+    public String createAccessToken(Member member) {
+        long expirationMillis = jwtTokenProperties.getAccessTokenExpirationMillis();
+        return createToken(member, expirationMillis, TokenType.ACCESS_TOKEN);
+    }
+
+    public String createRefreshToken(Member member) {
+        long expirationMillis = jwtTokenProperties.getRefreshTokenExpirationMillis();
+        return createToken(member, expirationMillis, TokenType.REFRESH_TOKEN);
+    }
+
+    public String createSignupToken(GoogleOauthInfoApiResponse oauthInfoApiResponse) {
+        long expirationMillis = jwtTokenProperties.getSignupTokenExpirationMillis();
+
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expirationMillis);
+
+        Map<String, Object> claims = Map.of(
+                "email", oauthInfoApiResponse.email(),
+                "picture", oauthInfoApiResponse.picture()
+        );
+
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .setSubject(SIGNUP_SUBJECT)
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .claim(jwtTokenProperties.TOKEN_TYPE, TokenType.SIGNUP_TOKEN)
+                .signWith(jwtTokenProperties.getSecretKey());
+        claims.forEach(jwtBuilder::claim);
+
+        return jwtBuilder.compact();
+    }
+
+    private String createToken(Member member, long expirationMillis, TokenType tokenType) {
+        Date now = new Date();
+        Date expirationDate = new Date(now.getTime() + expirationMillis);
+
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .setIssuedAt(now)
+                .setExpiration(expirationDate)
+                .claim(jwtTokenProperties.TOKEN_TYPE, tokenType.name())
+                .signWith(jwtTokenProperties.getSecretKey())
+                .compact();
+    }
+}
