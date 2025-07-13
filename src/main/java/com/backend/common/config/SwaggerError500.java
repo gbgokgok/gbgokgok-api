f0292bc6c3c01661b9283dc6552a114e2951223e
Jae-Young98 <ygy0102@gmail.com>
2025-07-13 16:19:07 +0900

    feat: 스웨거 공통 에러 응답 어노테이션 추가

---
+++ src/main/java/com/backend/common/config/SwaggerError500.java
@@ -0,0 +1,21 @@
+package com.backend.common.config;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.http.ProblemDetail;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponse(
+        responseCode = "500",
+        description = "내부 서버 오류",
+        content = @Content(schema = @Schema(implementation = ProblemDetail.class))
+)
+public @interface SwaggerError500 {
+
+}
